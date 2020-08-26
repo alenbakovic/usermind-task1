c1c4d746578930047e7ff2f6a5a9022236f9f189
Alen Bakovic <alen@getconvey.com>
2020-08-26 21:41:50 +0200

    Hash map implementation of task. Unit tests added.

---
+++ src/main/java/org/example/IndicesCalculator.java
@@ -0,0 +1,42 @@
+package org.example;
+import org.javatuples.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author alen bakovic (alen@getconvey.com)
+ */
+public class IndicesCalculator {
+
+    public List<Pair<Integer, Integer>> getIndices(int sum, int[] array) {
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+
+        Map<Integer, List<Integer>> indexedArray = getElementIndexes(array);
+        for (int i = 0; i < array.length; i++) {
+            if (indexedArray.get(sum - array[i]) != null) {
+                final int index = i; // to be used in lambda
+                indexedArray.get(sum - array[i])
+                        .stream()
+                        .filter(elementIndex -> index < elementIndex) // this will prevent duplicates
+                        .forEach(elementIndex -> result.add(Pair.with(index, elementIndex)));
+            }
+        }
+        return result;
+    }
+
+    private Map<Integer, List<Integer>> getElementIndexes(int[] array) {
+        Map<Integer, List<Integer>> indexes = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (indexes.get(array[i]) == null) {
+                indexes.put(array[i], new ArrayList<>(List.of(i)));
+            } else {
+                indexes.get(array[i]).add(i);
+            }
+        }
+        return indexes;
+    }
+}
---
+++ src/test/java/org/example/IndicesCalculatorTest.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author alen bakovic (alen@getconvey.com)
+ */
+public class IndicesCalculatorTest {
+
+    IndicesCalculator indicesCalculator = new IndicesCalculator();
+
+    @Test
+    public void test1() {
+        int [] input = new int[]{ 7, 5, -3, 15, 7 , 5, 5, 5, 5, -3 };
+        int sum = 12;
+
+        var result = indicesCalculator.getIndices(sum, input);
+        var expected = "[[0, 1], [0, 5], [0, 6], [0, 7], [0, 8], [1, 4], [2, 3], [3, 9], [4, 5], [4, 6], [4, 7], [4, 8]]";
+
+        Assert.assertEquals(result.toString(), expected);
+    }
+
+    @Test
+    public void test2() {
+        int [] input = new int[]{ 7, 5, 5 };
+        int sum = 12;
+
+        var result = indicesCalculator.getIndices(sum, input);
+        var expected = "[[0, 1], [0, 2]]";
+
+        Assert.assertEquals(result.toString(), expected);
+    }
+
+    @Test
+    public void test3() {
+        int [] input = new int[]{ 7, 5 };
+        int sum = 12;
+
+        var result = indicesCalculator.getIndices(sum, input);
+        var expected = "[[0, 1]]";
+
+        Assert.assertEquals(result.toString(), expected);
+    }
+
+    @Test
+    public void test4() {
+        int [] input = new int[]{ 1 };
+        int sum = 12;
+
+        var result = indicesCalculator.getIndices(sum, input);
+        var expected = "[]";
+
+        Assert.assertEquals(result.toString(), expected);
+    }
+
+    @Test
+    public void test5() {
+        int [] input = new int[]{ 7, 3, -3, 13, 13, -5, 15, -5, 3 };
+        int sum = 10;
+
+        var result = indicesCalculator.getIndices(sum, input);
+        var expected = "[[0, 1], [0, 8], [2, 3], [2, 4], [5, 6], [6, 7]]";
+
+        Assert.assertEquals(result.toString(), expected);
+    }
+}
